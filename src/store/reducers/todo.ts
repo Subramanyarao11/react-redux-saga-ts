e72ba08156e7b2b881991248f8e109392bc11755
SubramanyaRao11 <subramanya11rao@gmail.com>
2025-01-26 21:43:53 +0530

    Updated: reducer

--- src/store/reducers/todo.ts
+++ src/store/reducers/todo.ts
@@ -30,11 +30,11 @@ export default (state = initialState, action: TodoAction) =>
       case deleteTodo.TRIGGER:
       case toggleTodoStatus.TRIGGER: {
         draft.isLoading = true;
+        draft.error = null;
         break;
       }
 
       case getAllTodos.SUCCESS: {
-        draft.error = null; // Reset error on successful action to avoid showing stale error message
         if (action.payload && 'data' in action.payload && action.payload.data.todos) {
           draft.todos = action.payload.data.todos;
         }
@@ -42,7 +42,6 @@ export default (state = initialState, action: TodoAction) =>
       }
 
       case createTodo.SUCCESS: {
-        draft.error = null;
         if (action.payload && 'data' in action.payload && action.payload.data.todo) {
           draft.todos.push(action.payload.data.todo);
         }
@@ -51,7 +50,6 @@ export default (state = initialState, action: TodoAction) =>
 
       case updateTodo.SUCCESS:
       case toggleTodoStatus.SUCCESS: {
-        draft.error = null;
         if (action.payload && 'data' in action.payload && action.payload.data.todo) {
           const updatedTodo = action.payload.data.todo;
           const index = draft.todos.findIndex(todo => todo._id === updatedTodo._id);
@@ -63,7 +61,6 @@ export default (state = initialState, action: TodoAction) =>
       }
 
       case deleteTodo.SUCCESS: {
-        draft.error = null;
         if (action.payload && 'todoId' in action.payload) {
           const deletedTodoId = action.payload.todoId;
           const index = draft.todos.findIndex(todo => todo._id === deletedTodoId);
@@ -80,7 +77,6 @@ export default (state = initialState, action: TodoAction) =>
       case deleteTodo.FAILURE:
       case toggleTodoStatus.FAILURE: {
         if (action.payload && 'message' in action.payload) {
-          console.log('err reducer', action.payload.message);
           draft.error = action.payload.message;
         } else {
           draft.error = 'Something went wrong';
